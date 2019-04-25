46733630b444b04a748ef1d280d59e2010c313ff
Hobie Wetzels <hobie.wetzels@gmail.com>
2019-04-25 15:46:46 +0200

    created base

--- ronde2/schuinen_lijnen_logo_base - Copy/index/sketch.js
+++ ronde2/schuinen_lijnen_logo_base - Copy/index/sketch.js
@@ -6,25 +6,50 @@
 var mousexpos = 0;
 var mouseypos = 0;
 var rx;
-var numbSq = 1;
-var colorPick = 0;
+var numbSq = 30 + 1;
+var colorPick = 1;
 var rin = 50;
 var xinc = 10;
 var angle = 0;
 var speed;
 
 
+
 //create array
 var squares = new Array();
 var colors = ["#ffde4a", "#cf7fa1", "#d1b8c7"];
 
-for (i = 0; i < numbSq; i++) {
+squares.push({
+  //left under
+  x1: 0,
+  //right under
+  x2: 900,
+  //right upper
+  x3: 600,
+  //left upper
+  x4: 200,
+  color: colors[0]
+});
+
+for (i = 1; i < numbSq; i++) {
+  
+  if ( isOdd(i) == 1 ){
+    var width = 700 + Math.floor(Math.random() * 100);
+    var width2 = 1100 + Math.floor(Math.random() * 100);
+  }else{
+    var width2 = 700 + Math.floor(Math.random() * 100);
+    var width = 1100 + Math.floor(Math.random() * 100);
+  }
   squares.push({
-    x1: 0,
-    x2: 500,
-    x3: 200,
-    x4: 0,
-    color: colors[colorPick],
+    //left under
+    x1: squares[i-1].x1 - width,
+    //right under
+    x2: squares[i-1].x1,
+    //right upper
+    x3: squares[i-1].x4,
+    //left upper
+    x4: squares[i-1].x4 - width2,
+    color: colors[colorPick]
   });
 
 
@@ -54,8 +79,6 @@ function draw() {
 
     display(i);
 
-    //
-    quad(squares[i].x1, 250, squares[i].x2, 250, squares[i].x3, 0, squares[i].x4, 0);
 
     move(i);
     //intersect(i);
@@ -67,11 +90,18 @@ function draw() {
 //functions
 function display(i){
   fill(squares[i].color);
+  quad(squares[i].x1, 250, squares[i].x2, 250, squares[i].x3, 0, squares[i].x4, 0);
 }
 
 function move(i){
-
-  squares[i].x3 += 1;
+  //left under
+  squares[i].x1 += 2;
+  //right under
+  squares[i].x2 += 2;
+  //right upper
+  squares[i].x3 += 2;
+  //left upper
+  squares[i].x4 += 2;
 
 
 //reset x
@@ -79,12 +109,14 @@ if (squares[i].x > 970 + squares[i].r){
   squares[i].r = Math.floor(Math.random() * 500 + 100);
   squares[i].x = 0 - squares[i].r;
 }
-if (squares[i].r < 0){
-  squares[i].r = Math.floor(Math.random() * 500 + 100);
-  squares[i].x = 0 - squares[i].r;
-}
+
 //reset y
 
+}
+
+//odd
+function isOdd(num) { 
+  return num % 2;
 }
 
 
@@ -101,7 +133,6 @@ $( document ).ready(function() {
     mousexpos = event.pageX;
     mouseypos = event.pageY;
     speed = mousexpos - 480;
-    console.log(speed);
   });
 });
 
---
+++ ronde2/schuinen_lijnen_logo_base/index/sketch.js
@@ -0,0 +1,139 @@
+//yellow: #ffde4a
+//red: #e63a2b
+//blue: #84c497
+
+//global vars
+var mousexpos = 0;
+var mouseypos = 0;
+var rx;
+var numbSq = 30 + 1;
+var colorPick = 1;
+var rin = 50;
+var xinc = 10;
+var angle = 0;
+var speed;
+
+
+
+//create array
+var squares = new Array();
+var colors = ["#ffde4a", "#cf7fa1", "#d1b8c7"];
+
+squares.push({
+  //left under
+  x1: 0,
+  //right under
+  x2: 900,
+  //right upper
+  x3: 600,
+  //left upper
+  x4: 200,
+  color: colors[0]
+});
+
+for (i = 1; i < numbSq; i++) {
+  
+  if ( isOdd(i) == 1 ){
+    var width = 700 + Math.floor(Math.random() * 100);
+    var width2 = 1100 + Math.floor(Math.random() * 100);
+  }else{
+    var width2 = 700 + Math.floor(Math.random() * 100);
+    var width = 1100 + Math.floor(Math.random() * 100);
+  }
+  squares.push({
+    //left under
+    x1: squares[i-1].x1 - width,
+    //right under
+    x2: squares[i-1].x1,
+    //right upper
+    x3: squares[i-1].x4,
+    //left upper
+    x4: squares[i-1].x4 - width2,
+    color: colors[colorPick]
+  });
+
+
+  colorPick += 1;
+  if (colorPick == 3){
+    colorPick = 0;
+  }
+}
+
+console.log(squares);
+
+function setup() {
+
+  let myCanvas = createCanvas(970, 250);
+  myCanvas.parent('myContainer');
+  background('#cf7fa1');
+
+}
+
+function draw() {
+  background('#cf7fa1');
+  noStroke();
+  rectMode(RADIUS); 
+  for (i = 0; i < squares.length; i++) {
+
+    //functions
+
+    display(i);
+
+
+    move(i);
+    //intersect(i);
+
+  }
+  
+}
+
+//functions
+function display(i){
+  fill(squares[i].color);
+  quad(squares[i].x1, 250, squares[i].x2, 250, squares[i].x3, 0, squares[i].x4, 0);
+}
+
+function move(i){
+  //left under
+  squares[i].x1 += 2;
+  //right under
+  squares[i].x2 += 2;
+  //right upper
+  squares[i].x3 += 2;
+  //left upper
+  squares[i].x4 += 2;
+
+
+//reset x
+if (squares[i].x > 970 + squares[i].r){
+  squares[i].r = Math.floor(Math.random() * 500 + 100);
+  squares[i].x = 0 - squares[i].r;
+}
+
+//reset y
+
+}
+
+//odd
+function isOdd(num) { 
+  return num % 2;
+}
+
+
+function intersect(i){
+  if (Math.abs(squares[i].x - mousexpos) < squares[i].r)
+  {
+    squares[i].r -= 0.5;
+  }else{
+    squares[i].r += 0.2;
+  }
+}
+$( document ).ready(function() {
+  $( "#art" ).mousemove(function( event ) {
+    mousexpos = event.pageX;
+    mouseypos = event.pageY;
+    speed = mousexpos - 480;
+  });
+});
+
+
